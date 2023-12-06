889f38777043c985fa5208ff523f2a97cad0b30b
bruno-connectabil <pamoroso.bruno@gmail.com>
2023-12-06 19:29:03 -0300

    Solved double scroll

--- app/signup/layout.tsx
+++ app/signup/layout.tsx
@@ -3,7 +3,7 @@ import { Container } from "@mui/material";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <Container disableGutters={true} sx={{backgroundColor: "#EBF2F7"}} maxWidth={false}>
+    <Container disableGutters={true} sx={{backgroundColor: "#EBF2F7", overflow: "hidden"}} maxWidth={false}>
       {children}
     </Container>
   );
